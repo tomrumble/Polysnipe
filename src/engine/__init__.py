e745c066a69326beb14dabf60731d6fab577e4e7
Tom Rumble <thomas.rumble@gmail.com>
2026-03-07 18:24:14 +1100

    Fix integrated training refactor regressions

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,3 +1,9 @@
 from src.engine.research_engine import FixedTrainingConfig, ResearchEngine
+from src.engine.training_controller import TrainingController, TrainingLifecycleState
 
-__all__ = ["ResearchEngine", "FixedTrainingConfig"]
+__all__ = [
+    "ResearchEngine",
+    "FixedTrainingConfig",
+    "TrainingController",
+    "TrainingLifecycleState",
+]
